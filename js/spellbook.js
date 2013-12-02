fbbb64fc6d73cf7d1a7aa3314384382eee7eb4c3
Allard Hoeve <allard@byte.nl>
2013-12-02 19:45:08 +0100

    Add spell filter.
     - Add spell filter.
     - Add search clear button.
     - Closes #1.

--- js/spellbook.js
+++ js/spellbook.js
@@ -1,18 +1,71 @@
 
+
+var jQuerySelectors = function () {
+    // http://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
+    jQuery.expr[':'].textMatches = function(a, i, m) {
+        return jQuery(a).text().toUpperCase()
+            .indexOf(m[3].toUpperCase()) >= 0;
+    };
+};
+
+
 var SpellBook = function (options) {
     var select = options.selector;
 
     var collapse = function (e) {
+        e.preventDefault();
         $(this).find(".content").addClass("content-hidden");
         $(this).find(".icon-expand").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
         $(this).unbind('click').click(expand);
     }
 
     var expand = function (e) {
+        e.preventDefault();
         $(this).find(".content").removeClass("content-hidden");
         $(this).find(".icon-expand").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
         $(this).unbind('click').click(collapse);
     }
 
     $(select).click(expand);
-}
+};
+
+var SearchBar = function (options) {
+    var formSelector = options.selector;
+    var searchFieldSelector = "input#searchfield";
+
+    var getSearchField = function () {
+        return $($(searchFieldSelector).get(0));
+    };
+
+    // Event handler for form elements
+    var performSearch = function(e) {
+        e.preventDefault();
+        var input = getSearchField();
+
+        if (input.val() == '') {
+            $("#spell-wrapper").removeClass("search-filtered");
+            $(document).find("li.spell").removeClass("search-show");
+        }
+        else {
+            $("#spell-wrapper").addClass("search-filtered");
+            $(document).find("li.spell").removeClass("search-show");
+            $(document).find("li.spell:textMatches('" + input.val() + "')").addClass("search-show");
+        }
+    };
+
+    var addClearButton = function () {
+        var input = getSearchField();
+
+        input.wrap('<span class="search-clear" />');
+        input.after($('<span class="search-close">x</span>'));
+        input.parent().find(".search-close").click(function (e) {
+            input.val('').focus();
+            performSearch(e);
+        });
+    };
+
+    $(document).ready(function () {
+        $(formSelector).submit(performSearch);
+        addClearButton();
+    });
+};
